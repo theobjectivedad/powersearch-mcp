2b83916e51081e3f7e7883eb6b36d66eda4ece12
The Objective Dad <theobjectivedad@pm.me>
2025-12-26 22:45:44 -0600

    adding constraint on major version dependencies

--- src/powersearch_mcp/_version.py
+++ src/powersearch_mcp/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.5.dev9+g69a844eef.d20251227'
-__version_tuple__ = version_tuple = (0, 1, 5, 'dev9', 'g69a844eef.d20251227')
+__version__ = version = '0.1.5.dev19+gf6c48b14b.d20251227'
+__version_tuple__ = version_tuple = (0, 1, 5, 'dev19', 'gf6c48b14b.d20251227')
 
 __commit_id__ = commit_id = None
